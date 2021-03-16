d51954ba5a48e03f5db78e94c12cc7b34aafb3cb
joviannugeraha <joviandmi@gmail.com>
2021-03-16 17:33:31 +0700

    ada penambahan

--- my_first_dart_console/bin/perulanganwhile.dart
+++ my_first_dart_console/bin/perulanganwhile.dart
@@ -6,4 +6,12 @@ main(List<String> arguments) {
     print("halo ke " + i.toString());
     i += 1;
   }
+
+  print("===================");
+
+  int x = 0;
+  do {
+    print('dododo ke ' + x.toString());
+    x += 1;
+  } while (x < 5);
 }
